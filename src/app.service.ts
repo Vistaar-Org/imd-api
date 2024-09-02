3ed970aaf12d238b7626b1c2a8ab5b6ea6a8702b
Yash Mittal <yash@techsavvyash.dev>
2024-09-02 14:25:20 +0530

    fix: add provider id

--- src/app.service.ts
+++ src/app.service.ts
@@ -233,6 +233,7 @@ export class AppService {
         );
         console.log('sanitized rajkmai Data: ', sanitizedRAJAIData);
         rajaiItems = mapIMDItems(sanitizedRAJAIData);
+        rajaiItems.id = 'Raj Kisan APIs';
       } catch (err) {
         console.error(err);
         this.logger.error('Error fetching weather data from IMD', err);
