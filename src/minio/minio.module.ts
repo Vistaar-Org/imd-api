d5e440adced78cc10443faa1fd95a2f885676fd2
Yash Mittal <mittal.yash12@hotmail.com>
2025-01-19 14:19:25 +0530

    fix: port usage

--- src/minio/minio.module.ts
+++ src/minio/minio.module.ts
@@ -12,7 +12,7 @@ import { Client as MinioClient } from "minio";
       useFactory: (configService: ConfigService) => {
         return new MinioClient({
           endPoint: configService.get<string>('MINIO_ENDPOINT'),
-          port: parseInt(configService.get<string>('MINIO_PORT'), 10),
+          // port: parseInt(configService.get<string>('MINIO_PORT'), 10),
           useSSL: configService.get<string>('MINIO_USE_SSL') === 'true',
           accessKey: configService.get<string>('MINIO_ACCESS_KEY'),
           secretKey: configService.get<string>('MINIO_SECRET_KEY'),
